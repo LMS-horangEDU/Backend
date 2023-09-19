75b14cf2852c0b900cc7c6e0644649aeeda57b25
tastekim <tastekim@kakao.com>
2023-09-19 22:22:26 +0900

    :tada: 프로젝트 세팅

---
+++ app.ts
@@ -0,0 +1,41 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+// NODE_ENV 에 맞게 env 파일 설정.
+if (process.env.NODE_ENV === 'production') {
+  console.log(process.env.NODE_ENV);
+  dotenv.config({ path : path.join(__dirname, '/.env') });
+} else if (process.env.NODE_ENV === 'development') {
+  console.log(process.env.NODE_ENV);
+  dotenv.config({ path : path.join(__dirname, '/.env.local') });
+} else {
+  console.error('Not defined process.env.NODE_ENV');
+  throw new Error('Not defined process.env.NODE_ENV');
+}
+
+import Koa, { Context } from 'koa';
+import { koaBody } from 'koa-body';
+import cors from '@koa/cors';
+import mongodbConnect from './config/mongo_conf';
+import session from 'koa-session';
+
+const app = new Koa();
+
+app.use(cors({
+  origin : '*',
+  credentials : true,
+}));
+
+app.use(session(app));
+app.use(koaBody());
+
+app.on('error', (err: any, ctx: Context) => {
+  console.log(ctx.request.path);
+  console.error(err);
+});
+
+app.listen(process.env.PORT, async () => {
+  // mongoDB connection.
+  await mongodbConnect();
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
---
+++ config/mongo_conf.ts
@@ -0,0 +1,15 @@
+import { MongoClient } from 'mongodb';
+
+const { MONGODB_URI } = process.env;
+const client = new MongoClient(MONGODB_URI);
+
+export default async function run () {
+  try {
+    await client.connect();
+    console.log('Connected correctly MongoDB to server');
+  } catch (err) {
+    if (err instanceof Error) {
+      console.log(err.stack);
+    }
+  }
+}
